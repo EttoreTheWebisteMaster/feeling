75d02ea425032df5246cfa10167b8ae0059a4c6f
ettore-infn <ettore.serra@ba.infn.it>
2024-11-20 16:04:48 +0100

    fonts added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
-
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,15 +13,14 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn(
-          "bg-background min-h-screen font-sans antialiased",
-          inter.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  )
+		<html lang='en'>
+			<body
+				className={cn(
+					'bg-background min-h-screen antialiased'
+				)}
+			>
+				{children}
+			</body>
+		</html>
+  );
 }
