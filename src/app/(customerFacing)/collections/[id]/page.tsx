452abec9b650a4fdc4bad9f854aaccc951ce5925
ettore-infn <ettore.serra@ba.infn.it>
2024-11-28 11:09:09 +0100

    added collections page

---
+++ src/app/(customerFacing)/collections/[id]/page.tsx
@@ -0,0 +1,121 @@
+import PastCollections from '@/components/PastCollections';
+import { ProductCard, ProductCardSkeleton } from '@/components/ProductCard';
+import VideoPlayer from '@/components/VideoPlayer';
+import db from '@/db/db';
+import { cache } from '@/lib/cache';
+import { Collection, Product } from '@prisma/client';
+import { Suspense } from 'react';
+
+// Fetch and cache the collection for use on the homepage
+const getCollection = cache(
+	async (collectionId: string) => {
+		return await db.collection.findUnique({
+			where: { isAvailable: true, id: collectionId },
+		});
+	},
+	['/', 'getCollection']
+);
+
+// Fetch and cache the newest products from the collection
+const getProducts = cache(
+	async (collectionId: string) => {
+		const collection = await getCollection(collectionId);
+
+		// If there's no available collection, return an empty array
+		if (!collection) return [];
+
+		return db.product.findMany({
+			where: {
+				isAvailable: true,
+				collectionId: collection.id,
+			},
+			orderBy: { createdAt: 'desc' },
+		});
+	},
+	['/', 'getProducts']
+);
+
+export default async function CollectionPage({
+	params: { id },
+}: Readonly<{
+	params: { id: string };
+}>) {
+	return (
+		<main className='space-y-12'>
+			{/* Pass a function that fetches the collection */}
+			<CollectionHeaderSection
+				collectionFetcher={() => getCollection(id)}
+			/>
+			{/* Pass a function that fetches the products */}
+			<ProductGridSection
+				title='Newest products'
+				productsFetcher={() => getProducts(id)}
+			/>
+			{/* TODO in future */}
+			{/* <PastCollections /> */}
+		</main>
+	);
+}
+
+type CollectionHeaderSectionProps = {
+	collectionFetcher: () => Promise<Collection | null>;
+};
+
+async function CollectionHeaderSection({
+	collectionFetcher,
+}: Readonly<CollectionHeaderSectionProps>) {
+	const collection = await collectionFetcher();
+
+	// Display a fallback message if there’s no available collection
+	if (!collection) {
+		return <div>Not available at the moment.</div>;
+	}
+
+	return (
+		<section className='collection-header'>
+			<VideoPlayer
+				collection={collection}
+				videoPath={collection.videoPath}
+			/>
+		</section>
+	);
+}
+
+type ProductGridSectionProps = {
+	title: string;
+	productsFetcher: () => Promise<Product[]>;
+};
+
+function ProductGridSection({
+	productsFetcher,
+	title,
+}: Readonly<ProductGridSectionProps>) {
+	return (
+		<div className='space-y-4 flex justify-center'>
+			<div className='grid grid-cols-1 lg:grid-cols-2 gap-3 px-2'>
+				<Suspense
+					fallback={
+						<>
+							<ProductCardSkeleton />
+							<ProductCardSkeleton />
+							<ProductCardSkeleton />
+							<ProductCardSkeleton />
+						</>
+					}
+				>
+					<ProductSuspense productsFetcher={productsFetcher} />
+				</Suspense>
+			</div>
+		</div>
+	);
+}
+
+async function ProductSuspense({
+	productsFetcher,
+}: {
+	productsFetcher: () => Promise<Product[]>;
+}) {
+	return (await productsFetcher()).map((product) => (
+		<ProductCard key={product.id} {...product} />
+	));
+}
--- src/app/(customerFacing)/layout.tsx
+++ src/app/(customerFacing)/layout.tsx
@@ -1,30 +1,63 @@
 import { Nav, NavLink, SubNavLink, NavCollapse } from '@/components/Nav';
 import Footer from '@/components/Footer';
+import db from '@/db/db';
+import { cache } from '@/lib/cache';
 
 export const dynamic = 'force-dynamic';
 
-export default function Layout({
+const getCollections = cache(async () => {
+	return await db.collection.findMany({
+		where: { isAvailable: true },
+		orderBy: { createdAt: 'desc' },
+	});
+}, ['/', 'getCollections']);
+
+function formatToSeasonalDate(dateString: string): string {
+	const date = new Date(dateString);
+	const year = date.getUTCFullYear();
+
+	// Get the month (0-indexed, so January is 0 and December is 11)
+	const month = date.getUTCMonth();
+
+	// Determine the season based on the month
+	let season = ''
+
+	if (month >= 2 && month <= 4) {
+		season = 'spring';
+	} else if (month >= 5 && month <= 7) {
+		season = 'summer';
+	} else if (month >= 8 && month <= 10) {
+		season = 'fall';
+	} else {
+		season = 'winter';
+	}
+
+	return `${season} ${year}`;
+}
+
+export default async function Layout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const collections = await getCollections();
+
 	return (
 		<>
 			<Nav>
 				<NavLink href='/'>Home</NavLink>
 				<NavCollapse title='Collections'>
-					<SubNavLink href='/' date='summer 2024'>
-						Neon
-					</SubNavLink>
-					<SubNavLink href='/' date='summer 2024'>
-						Name 1
-					</SubNavLink>
-					<SubNavLink href='/' date='summer 2024'>
-						Name 2 super lungo che non cè più spazio per scrivere
-					</SubNavLink>
-					<SubNavLink href='/' date='summer 2024'>
-						Name 3
-					</SubNavLink>
+					{collections.map((collection) => (
+						<SubNavLink
+							key={collection.id}
+							href={`/collections/${collection.id}`}
+							date={formatToSeasonalDate(
+								collection.createdAt.toString()
+							)}
+						>
+							{collection.name}
+						</SubNavLink>
+					))}
 				</NavCollapse>
 				<NavLink href='/products'>Products</NavLink>
 				<NavLink href='/orders'>My Orders</NavLink>
